3cf776966ba5668fa2a58578541b3c07b6a7e54a
TheDodo24 <dodogaming.info@gmail.com>
2023-06-21 11:08:08 +0200

    Refactor

---
+++ static/js/nav.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+  let current = window.location.pathname;
+  let links = this.document.getElementsByClassName("link");
+  console.log(current);
+  for (var i = 0; i < links.length; i++) {
+    var link = links.item(i);
+    if (link.classList.contains("underline")) {
+      link.classList.remove("underline");
+    }
+    if (link.getAttribute("href") == current) {
+      link.classList.add("underline");
+    }
+  }
+  if (current == "/smallmatch.html" || current == "/bigmatch.html") {
+    let dropBtns = this.document.getElementsByClassName("dropbtn");
+    dropBtns[0].classList.add("underline");
+  }
+});
