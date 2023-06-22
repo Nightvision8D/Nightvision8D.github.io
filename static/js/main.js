7567837f216b5551a06a506935b00eea092fface
TheDodo24 <dodogaming.info@gmail.com>
2023-06-22 21:52:52 +0200

    Add media support on index

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,7 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
-  header.classList.toggle("sticky", window.scrollY > 910);
+  var image = document.querySelector(".bgimg-1");
+  if (this.window.innerWidth >= 620) {
+    header.classList.toggle("sticky", window.scrollY > image.clientHeight);
+  }
 });
