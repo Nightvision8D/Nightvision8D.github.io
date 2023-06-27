95b594e6d56190ebcf1c18d3c1392dda3e42328c
TheDodo24 <domenic.roos@icloud.com>
2023-06-27 15:28:57 +0200

    Add comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// check if scroll is bigger than image size to add sticky class to navbar
 window.addEventListener("scroll", function () {
   var header = document.querySelector(".header");
   header.classList.toggle("sticky", window.scrollY > 910);
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,21 +1,25 @@
+// check site to add underline class to list element in navbar
 window.addEventListener("load", function () {
   let current = window.location.pathname;
   let links = this.document.getElementsByClassName("link");
-  console.log(current);
 
   for (var i = 0; i < links.length; i++) {
     var link = links.item(i);
+
     if (link.classList.contains("underline")) {
       link.classList.remove("underline");
     }
+
     if (link.getAttribute("href") == current.replace("/", "")) {
       link.classList.add("underline");
     }
   }
+
   if (current == "/smallmatch.html" || current == "/bigmatch.html") {
     let dropBtns = this.document.getElementsByClassName("dropbtn");
     dropBtns[0].classList.add("underline");
   }
+
   if (current.startsWith("/match")) {
     let dropBtns = this.document.getElementsByClassName("dropbtn");
     dropBtns[1].classList.add("underline");
